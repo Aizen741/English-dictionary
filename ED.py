f6c9e1b0a284a83ef3e6b59e9b2dde1866c405e7
Aizen741 <rahulsivadas2009@gmail.com>
2019-07-15 00:39:21 +0530

    Code-4

--- ED.py
+++ ED.py
@@ -1,13 +1,18 @@
 import json
 from difflib import get_close_matches
 
-data = json.load(open("data.json"))
 
+data = json.load(open("data.json"))
 def translate(w):
     w = w.lower()
     if w in data :
         return data[w]
+# Added to deal with input words which starts with capital letter
+    elif w.title() in data:
+        return data[w.title()]
+#will check the close range words
     elif len(get_close_matches(w,data.keys())) >0:
+#Condition given to the user
         yn = input("Did you mean %s instead? Enter Y if yes or N for no: " %get_close_matches(w,data.keys())[0])
         if yn == 'Y':
             return data[get_close_matches(w,data.keys())[0]]
@@ -17,6 +22,13 @@ def translate(w):
             return "We didn't understand your entry."
     else:
         return "The word doesn't exit, Please re-check it!"
+
 word = input("Enter word:")
 
-print(translate(word))
+output = translate(word)
+
+if type(output) == list:
+    for item in output:
+        print(item)
+else:
+    print(output)
